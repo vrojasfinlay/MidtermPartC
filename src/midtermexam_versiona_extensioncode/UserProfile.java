06b91ee40d68068a8d925495244a8381e4642e81
vrojasfinlay <122639111+vrojasfinlay@users.noreply.github.com>
2023-02-24 12:14:57 -0500

    Update UserProfile.java

--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -5,8 +5,7 @@
  */
 package midtermexam_versiona_extensioncode;
 //DONE!!
-//adding these comments for question # 5 of midterm!!!!
-/**
+/**adding these comments for question # 5 of midterm!!1
  * The user profile for the OnlineVids system. The user has a userID
  * and a favourite genre that must be one of the choices in the
  * genres array. This code is to be used for SYST 17796 midterm exam.
